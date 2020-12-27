44fa7b93fd542b9bc4c42737278a102261381bd7
gerlik <gerliks@gmail.com>
2020-12-28 01:37:27 +0200

    Add two dictionaries, finish starting dialogue with user

--- src/main/scala/game/Main.scala
+++ src/main/scala/game/Main.scala
@@ -1,4 +1,5 @@
 import scala.io.StdIn.readInt
+import scala.util.Random
 
 object Other {
 
@@ -9,30 +10,44 @@ object Main {
   // TODO upper/lowecase?
   val consonants = List('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z')
   val vowels = List('a', 'e', 'i', 'o', 'u')
+  val random = new Random
 
-  def askCharNumbers(): (Int, Int) = {
-    Console.println("Insert no of consonants: ")
-    val cons = readInt() // Assumes that user inserts integers
-    Console.println("Insert no of vowels: ")
-    val vowels = readInt()
-    if (cons < 4 || vowels < 3) {
-      Console.println("There has to be at least 4 consonants, you entered: " + cons)
-      Console.println("There has to be at least 3 vowels, you entered: " + vowels)
-      askCharNumbers()
-    }
-    if (cons + vowels < 9) {
-      val characters = cons + vowels
-      Console.println("There has to be at least 9 chatacters! You have: " + characters)
-      askCharNumbers()
-    }
-    (cons, vowels)
+  def returnConsonants(arv: Int): Unit = { // Random consonants and vowels
+
   }
 
+  def returnVowels(arv: Int): Unit = {
+
+  }
+
+  def askCharNumbers(): (Int, Int) = {
+    Console.println("Enter no of consonants: ")
+    val consonant = readInt() // Assumes that user inserts integers
+    Console.println("Enter no of vowels: ")
+    val vowel = readInt()
+    if (consonant < 4 || vowel < 3) {
+      Console.println("There needs to be at least 4 consonants, you entered: " + consonant)
+      Console.println("There needs to be at least 3 vowels, you entered: " + vowel)
+      askCharNumbers()
+    }
+    if (consonant + vowel < 9) {
+      val chars = consonant + vowel
+      Console.println("There has to be at least 9 characters! You have: " + chars)
+      askCharNumbers()
+    }
+    (consonant, vowel)
+  }
+
+  def showChars(characters: (Int, Int)): Unit = {
+    returnConsonants(characters._1)
+    returnVowels(characters._2)
+  }
 
   def main(args: Array[String]): Unit = {
-    Console.println("Welcome to Countdown! Choose 9 characters")
+    Console.println("Welcome to Countdown! Choose your 9 characters: ")
     val chars = askCharNumbers()
     Console.println("\nYou chose " + chars._1 + " consonants and " + chars._2 + " vowels")
+    showChars(chars)
 
   }
 
